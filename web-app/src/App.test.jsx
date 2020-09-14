27d55dd25781a06d29ec4a3696a56d1fe14cda52
Erin Eland <erin_168@icloud.com>
2020-09-14 18:03:39 +0100

    Loading HomePage component into main App page.

--- web-app/src/App.test.jsx
+++ web-app/src/App.test.jsx
@@ -5,5 +5,5 @@ import App from './App'
 it('renders title text', () => {
   const { getByText } = render(<App />)
 
-  expect(getByText('Home')).toBeInTheDocument()
+  expect(getByText('Regular Payments')).toBeInTheDocument()
 })
