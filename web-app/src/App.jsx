c6dc409db89ec6666f5fc37fe396d1d459fe52ab
Erin Eland <erin@erineland.com>
2020-09-14 19:03:21 +0100

    Using HomePage component to show list of bills.

--- web-app/src/App.jsx
+++ web-app/src/App.jsx
@@ -16,36 +16,7 @@ export default () => (
     <Router>
       <Switch>
         <Route exact path="/">
-          <BillPaymentsList
-            bills={
-              [
-                {
-                    name: 'Rent',
-                    price: '£500',
-                    frequency: 'Monthly',
-                    nextOccurringDate: '7th January, 2021'
-                },
-                {
-                    name: 'Gym Membership',
-                    price: '£19',
-                    frequency: 'Monthly',
-                    nextOccurringDate: '1st February, 2021'
-                },
-                {
-                    name: 'TV License',
-                    price: '£157.50',
-                    frequency: 'Annual',
-                    nextOccurringDate: '28th April, 2021'
-                },
-                {
-                    name: 'Dog Walker',
-                    price: '£20',
-                    frequency: 'Weekly',
-                    nextOccurringDate: '7th January, 2021'
-                },
-            ]
-            }
-          />
+          <HomePage />
         </Route>
         <Route path="/add_payment">
           <Example />
--- web-app/src/components/HomePage/HomePage.jsx
+++ web-app/src/components/HomePage/HomePage.jsx
@@ -1,14 +1,46 @@
 import React from 'react'
 import TitleBanner from '../TitleBanner/TitleBanner';
+import BillPaymentsList from '../BillPaymentsList/BillPaymentsList';
 import {
     Link,
 } from 'react-router-dom'
 
-const Home = () => (
-    <div>
+const Home = () => {
+  const bills =  [
+    {
+        name: 'Rent',
+        price: '£500',
+        frequency: 'Monthly',
+        nextOccurringDate: '7th January, 2021'
+    },
+    {
+        name: 'Gym Membership',
+        price: '£19',
+        frequency: 'Monthly',
+        nextOccurringDate: '1st February, 2021'
+    },
+    {
+        name: 'TV License',
+        price: '£157.50',
+        frequency: 'Annual',
+        nextOccurringDate: '28th April, 2021'
+    },
+    {
+        name: 'Dog Walker',
+        price: '£20',
+        frequency: 'Weekly',
+        nextOccurringDate: '7th January, 2021'
+    },
+  ];
+
+  return (
+    <div className="home-page__container">
       <TitleBanner title="Regular Payments" />
-      <Link to="/example">Go to /example</Link>
+      <BillPaymentsList
+            bills={bills}
+          />
     </div>
-)
+  )
+}
 
 export default Home;
