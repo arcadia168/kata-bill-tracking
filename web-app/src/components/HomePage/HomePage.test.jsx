3ffea901794816212c7539de3232e5ef9a361478
Erin Eland <erin@erineland.com>
2020-09-15 01:13:14 +0100

    Fixing failing unit tests.

--- web-app/src/components/HomePage/HomePage.test.jsx
+++ web-app/src/components/HomePage/HomePage.test.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import HomePage from './HomePage'
-import {
-    BrowserRouter as Router,
-    Route,
-} from 'react-router-dom'
-import { Provider, useDispatch, useSelector } from 'react-redux'
+import { Provider } from 'react-redux'
 import store from '../../state/store'
 
 describe('Home page', () => {
