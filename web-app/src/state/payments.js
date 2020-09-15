e577d922cddaf0522c3f2c949a6c9b319f6ba79f
Erin Eland <erin_168@icloud.com>
2020-09-15 11:25:09 +0100

    Using createAsyncThunk for updating a bill payment. Moving async logic out of component into Redux reducers.

--- web-app/src/state/payments.js
+++ web-app/src/state/payments.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from 'axios';
+import axios from "axios";
 
 export const fetchBillPayments = createAsyncThunk(
   "payments/billPaymentsLoading",
@@ -10,6 +10,26 @@ export const fetchBillPayments = createAsyncThunk(
     } catch (error) {
       console.error(
         `An error occurred fetching bills from the server: ${JSON.stringify(
+          error
+        )}`
+      );
+    }
+  }
+);
+
+export const updateBillPayment = createAsyncThunk(
+  "payments/updateBillPayment",
+  async (updatedBillPayment) => {
+    debugger;
+    try {
+      const response = await axios.patch(
+        `http://localhost:8080/payments/${updatedBillPayment.id}`,
+        updatedBillPayment
+      );
+      return response.data;
+    } catch (error) {
+      console.error(
+        `An error occurred updatin a bill on the server: ${JSON.stringify(
           error
         )}`
       );
@@ -67,6 +87,21 @@ const paymentsSlice = createSlice({
       state.payments = action.payload;
     },
     [fetchBillPayments.rejected.type]: (state, action) => {},
+    [updateBillPayment.pending.type]: (state, action) => {},
+    [updateBillPayment.fulfilled.type]: (state, action) => {
+      console.info(`updatePayment.fulfilled.type action handler invoked with paylod:`);
+      console.log(action.payload);
+
+      const newUpdatedPayments = state.payments.map((payment) => {
+        if (payment.id === action.payload.id) {
+          return action.payload;
+        }
+        return payment;
+      });
+
+      state.payments = newUpdatedPayments;
+    },
+    [updateBillPayment.rejected.type]: (state, action) => {},
   },
 });
 
