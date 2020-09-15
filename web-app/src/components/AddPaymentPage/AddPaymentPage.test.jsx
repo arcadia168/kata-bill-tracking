b1e40ba5de3b982265cfc23a295a50818700219c
Erin Eland <erin_168@icloud.com>
2020-09-15 01:13:14 +0100

    Fixing failing unit tests.

--- web-app/src/components/AddPaymentPage/AddPaymentPage.test.jsx
+++ web-app/src/components/AddPaymentPage/AddPaymentPage.test.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import AddPaymentPage from './AddPaymentPage';
+import { Provider } from 'react-redux'
+import store from '../../state/store'
 
 describe('Add Payment Page', () => {
     it('Renders the name of the page', () => {
         const { getByText } = render(
-            <AddPaymentPage />
+            <Provider store={store}>
+                <AddPaymentPage />
+            </Provider>
         )
 
         expect(getByText('Add A Bill')).toBeInTheDocument()
@@ -15,7 +19,9 @@ describe('Add Payment Page', () => {
 
     it('Renders button to add new payment', () => {
         const { getByText } = render(
-            <AddPaymentPage />
+            <Provider store={store}>
+                <AddPaymentPage />
+            </Provider>
         )
 
         expect(getByText('Add new payment')).toBeInTheDocument()
